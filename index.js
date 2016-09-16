53d5db2d9b41b8a36c09cb44d13fbea6ee54ee9d
Alexandra Breuhaus <sbreuhaus@gmail.com>
2016-09-16 14:00:58 -0400

    css selectors

---
+++ index.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $('.checkbox').click(function(){
+    let site = $('input[name^=site]');
+    let mailing = $('input[name^=mailing]');
+    let clientName = $('input[name^=client_name]');
+    let mailingName = $('input[name=mailing_address_name]');
+  })
+})
