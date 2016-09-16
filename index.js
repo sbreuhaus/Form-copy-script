65cf5847959844dfcb30eab0ff545dde746e5e95
Alexandra Breuhaus <sbreuhaus@gmail.com>
2016-09-16 14:39:31 -0400

    loop added

--- index.js
+++ index.js
@@ -4,5 +4,14 @@ $(document).ready(function(){
     let mailing = $('input[name^=mailing]');
     let clientName = $('input[name^=client_name]');
     let mailingName = $('input[name=mailing_address_name]');
+    if($('#isSelected').prop('checked') == false){
+      let form = document.getElementById('my-form');
+      form.reset();
+    } else {
+      mailingName[0].value = clientName[0].value;
+      for (var i = 0; i < site.length-1; i++) {
+        mailing[i+1].value = site[i].value;
+      }
+    }
   })
 })
